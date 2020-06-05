c32c7d77093ba24773ae5917d600a78edc751e19
tony <cardiox1212@hotmail.fr>
2020-06-05 17:02:19 +0200

    Norm export.c

--- srcs/export/export.c
+++ srcs/export/export.c
@@ -6,7 +6,7 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/31 04:05:50 by user42            #+#    #+#             */
-/*   Updated: 2020/06/01 00:13:49 by bbellavi         ###   ########.fr       */
+/*   Updated: 2020/06/05 16:59:30 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ t_bitmap	create_bitmap(t_vec res)
 
 	bitmap = (t_bitmap){
 		.bf_type = MAGIC_VALUE,
-		.bf_size = res.x * res.y + BMP_IMAGE_HEADER_SIZE + BMP_FILES_HEADER_SIZE,
+		.bf_size = res.x * res.y +
+		BMP_IMAGE_HEADER_SIZE + BMP_FILES_HEADER_SIZE,
 		.bf_reserved1 = 0,
 		.bf_reserved2 = 0,
 		.bf_offset_bits = 54,
@@ -37,12 +38,13 @@ t_bitmap	create_bitmap(t_vec res)
 	return (bitmap);
 }
 
-int		export_to_bmp(const char *filename, t_game *data)
+int			export_to_bmp(const char *filename, t_game *data)
 {
-	const int		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, S_IRWXU);
 	const t_bitmap	bmp = create_bitmap(data->map.resolution);
+	int				fd;
 	t_vec			i;
 
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, S_IRWXU);
 	if (fd < 0)
 		return (1);
 	write(fd, &bmp, sizeof(bmp));
@@ -50,7 +52,10 @@ int		export_to_bmp(const char *filename, t_game *data)
 	while (i.y < data->map.resolution.y)
 	{
 		i.x = (data->map.resolution.y - i.y - 1) * data->map.resolution.x;
-		write(fd, &data->image.img_data_addr[i.x], sizeof(unsigned int) * data->map.resolution.x);
+		write(
+			fd,
+			&data->image.img_data_addr[i.x],
+			sizeof(unsigned int) * data->map.resolution.x);
 		i.y++;
 	}
 	return (0);
