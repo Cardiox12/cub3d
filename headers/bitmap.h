a1ad7a2f0f02b6801d5f1a40767805b3ac5b485a
tony <cardiox1212@hotmail.fr>
2020-06-05 14:30:54 +0200

    Norm bitmap.h

--- headers/bitmap.h
+++ headers/bitmap.h
@@ -6,21 +6,21 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/31 04:07:30 by bbellavi          #+#    #+#             */
-/*   Updated: 2020/06/01 02:44:55 by bbellavi         ###   ########.fr       */
+/*   Updated: 2020/06/05 14:30:35 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_BITMAP_H
 # define FT_BITMAP_H
 
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/uio.h>
-#include <unistd.h>
-#include <stdint.h>
-#include "types.h"
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdint.h>
+# include "types.h"
 
-#pragma pack(push, 1)
+# pragma pack(push, 1)
 
 typedef struct	s_bitmap
 {
@@ -29,7 +29,6 @@ typedef struct	s_bitmap
 	u_int16_t	bf_reserved1;
 	u_int16_t	bf_reserved2;
 	uint32_t	bf_offset_bits;
-
 	uint32_t	bi_size;
 	uint32_t	bi_width;
 	uint32_t	bi_height;
@@ -43,7 +42,7 @@ typedef struct	s_bitmap
 	uint32_t	bi_clr_important;
 }				t_bitmap;
 
-#pragma pack(pop)
+# pragma pack(pop)
 
 # define BMP_IMAGE_HEADER_SIZE 40
 # define BMP_FILES_HEADER_SIZE 14
@@ -51,6 +50,6 @@ typedef struct	s_bitmap
 # define MAGIC_VALUE 0x4d42
 # define SAVE_NAME "save.bmp"
 
-int		export_to_bmp(const char *filename, t_game *data);
+int				export_to_bmp(const char *filename, t_game *data);
 
 #endif
