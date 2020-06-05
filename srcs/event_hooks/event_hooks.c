506113f8362826e284192a602e9b3e4a8ac0bf09
tony <cardiox1212@hotmail.fr>
2020-06-05 16:52:28 +0200

    Norm event_hooks.c

--- srcs/event_hooks/event_hooks.c
+++ srcs/event_hooks/event_hooks.c
@@ -6,25 +6,21 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/26 00:44:00 by bbellavi          #+#    #+#             */
-/*   Updated: 2020/06/05 15:45:18 by bbellavi         ###   ########.fr       */
+/*   Updated: 2020/06/05 16:52:01 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include "cub3d.h"
 #include "event_hooks.h"
 #include "bitmap.h"
 
 static void	quit(t_game *data)
 {
-	mlx_destroy_image(
-		data->infos.mlx_ptr,
-		data->image.img_ref
-	);
+	mlx_destroy_image(data->infos.mlx_ptr, data->image.img_ref);
 	exit(0);
 }
 
-int		key_pressed(int keycode, t_game *data)
+int			key_pressed(int keycode, t_game *data)
 {
 	if (keycode == KEY_ESC)
 		quit(data);
@@ -32,20 +28,20 @@ int		key_pressed(int keycode, t_game *data)
 	return (0);
 }
 
-int		key_released(int keycode, t_game *data)
+int			key_released(int keycode, t_game *data)
 {
 	(void)data;
 	g_event_array[keycode] = FALSE;
 	return (0);
 }
 
-int		exit_hook(t_game *data)
+int			exit_hook(t_game *data)
 {
 	quit(data);
 	return (0);
 }
 
-int		motion_hook(int x, int y, t_game *data)
+int			motion_hook(int x, int y, t_game *data)
 {
 	(void)data;
 	(void)x;
