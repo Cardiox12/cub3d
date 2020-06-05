09c41634096338e4a1a6ca080c67e21e74d866ec
tony <cardiox1212@hotmail.fr>
2020-06-05 16:00:14 +0200

    Norm graphics utils

--- headers/graphics_utils.h
+++ headers/graphics_utils.h
@@ -6,16 +6,17 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/25 01:14:03 by bbellavi          #+#    #+#             */
-/*   Updated: 2020/05/18 17:57:10 by bbellavi         ###   ########.fr       */
+/*   Updated: 2020/06/05 15:58:19 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#   ifndef FT_LIBGL_H
-#   define FT_LIBGL_H
+#ifndef FT_GRAPHICS_UTILS_H
+# define FT_GRAPHICS_UTILS_H
 
-#include "cub3d.h"
-#include "render.h"
+# include "cub3d.h"
+# include "render.h"
 
-unsigned int	ft_encode_rgb(unsigned char red, unsigned char green, unsigned char blue);
+unsigned int
+ft_encode_rgb(unsigned char red, unsigned char green, unsigned char blue);
 
-#   endif
+#endif
