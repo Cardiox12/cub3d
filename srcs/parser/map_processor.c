f2a5970620e72ea6155d268a460a88d0d893428a
tony <cardiox1212@hotmail.fr>
2020-06-05 22:08:02 +0200

    Norm map_processor.c

--- srcs/parser/map_processor.c
+++ srcs/parser/map_processor.c
@@ -6,7 +6,7 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/20 22:39:22 by bbellavi          #+#    #+#             */
-/*   Updated: 2020/06/05 16:03:46 by bbellavi         ###   ########.fr       */
+/*   Updated: 2020/06/05 22:07:39 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,15 @@ int			map_processor(t_game *data)
 	{
 		curlen = ft_strlen(data->map.map[i.y]);
 		if (curlen < (size_t)data->map.map_xsize)
-			data->map.map[i.y] = ft_realloc(data->map.map[i.y], curlen, data->map.map_xsize + 1);
+			data->map.map[i.y] = ft_realloc(data->map.map[i.y], curlen,
+			data->map.map_xsize + 1);
 		i.x = 0;
 		while (i.x < data->map.map_xsize)
 		{
 			if (ft_strchr(MAP_CHARSET_EXTEND, data->map.map[i.y][i.x]) == NULL)
 				return (CODE_ERR_BAD_CHAR_IN_MAP);
-			if (data->map.map[i.y][i.x] == SPACE || data->map.map[i.y][i.x] == '\0')
+			if (data->map.map[i.y][i.x] == SPACE ||
+			data->map.map[i.y][i.x] == '\0')
 				data->map.map[i.y][i.x] = PREPROCESS_REPLACE_COLOR;
 			i.x++;
 		}
@@ -54,11 +56,10 @@ int			flood_fill(t_game *data, t_stack **stack, char **map)
 	index = 0;
 	while (index < DELTA_SIZE)
 	{
-		n = (t_vec){
-			pos.x + (int[DELTA_SIZE]){0, 1, 0, -1}[index], 
-			pos.y + (int[DELTA_SIZE]){-1, 0, 1, 0}[index]
-		};
-		if (n.x >= 0 && n.x < data->map.map_xsize && n.y >= 0 && n.y < data->map.map_ysize)
+		n = (t_vec){pos.x + (int[DELTA_SIZE]){0, 1, 0, -1}[index],
+		pos.y + (int[DELTA_SIZE]){-1, 0, 1, 0}[index]};
+		if (n.x >= 0 && n.x < data->map.map_xsize
+		&& n.y >= 0 && n.y < data->map.map_ysize)
 		{
 			if (ft_strchr(TARGET_COLORS, map[n.y][n.x]) != NULL)
 				stack_push(stack, n);
@@ -73,14 +74,14 @@ int			flood_fill(t_game *data, t_stack **stack, char **map)
 	return (TRUE);
 }
 
-int		map_is_valid(t_game *data)
+int			map_is_valid(t_game *data)
 {
 	t_stack			*stack;
 	char			**map;
 	unsigned int	errors;
 
 	stack = NULL;
-	map  = string_array_copy(data->map.map, data->map.map_ysize);
+	map = string_array_copy(data->map.map, data->map.map_ysize);
 	if ((errors = get_starting_point(data)))
 	{
 		string_array_free(map, data->map.map_ysize);
@@ -96,7 +97,7 @@ int		map_is_valid(t_game *data)
 	return (RET_NO_ERROR);
 }
 
-int	check_eof(t_game *data, int fd)
+int			check_eof(t_game *data, int fd)
 {
 	if (data->map.line != NULL)
 	{
