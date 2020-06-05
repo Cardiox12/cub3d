7213c3bb036feb22ebf0594a5c97ff6c6aee9ff2
tony <cardiox1212@hotmail.fr>
2020-06-05 22:04:22 +0200

    Norm errors.c

--- srcs/parser/errors.c
+++ srcs/parser/errors.c
@@ -6,7 +6,7 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/22 15:45:25 by bbellavi          #+#    #+#             */
-/*   Updated: 2020/06/05 16:06:31 by bbellavi         ###   ########.fr       */
+/*   Updated: 2020/06/05 22:04:02 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int		errors_print(unsigned int errors, int listall)
 			ft_putstr(g_errors_array[index].strerror);
 			ft_putchar('\n');
 			if (!listall)
-				return g_errors_array[index].mask;
+				return (g_errors_array[index].mask);
 		}
 		index++;
 	}
