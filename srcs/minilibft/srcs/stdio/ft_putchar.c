719c1b939d3a6eca111ad47736f2c39c2cbb68ec
tony <cardiox1212@hotmail.fr>
2020-06-06 02:26:49 +0200

    Norm stdio functions

--- srcs/minilibft/srcs/stdio/ft_putchar.c
+++ srcs/minilibft/srcs/stdio/ft_putchar.c
@@ -6,13 +6,13 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/26 01:24:11 by bbellavi          #+#    #+#             */
-/*   Updated: 2020/01/26 01:27:36 by bbellavi         ###   ########.fr       */
+/*   Updated: 2020/06/06 02:25:54 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_stdio.h"
 
-void    ft_putchar(char c)
+void	ft_putchar(char c)
 {
-    write(1, &c, 1);
+	write(1, &c, 1);
 }
--- srcs/minilibft/srcs/stdio/ft_putstr.c
+++ srcs/minilibft/srcs/stdio/ft_putstr.c
@@ -6,17 +6,17 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/26 01:27:45 by bbellavi          #+#    #+#             */
-/*   Updated: 2020/01/26 01:29:31 by bbellavi         ###   ########.fr       */
+/*   Updated: 2020/06/06 02:26:28 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_stdio.h"
 
-void    ft_putstr(const char *s)
+void	ft_putstr(const char *s)
 {
-    const char *final_s = s;
+	const char *final_s = s;
 
-    while (*s)
-        s++;
-    write(1, final_s, s - final_s);
+	while (*s)
+		s++;
+	write(1, final_s, s - final_s);
 }
